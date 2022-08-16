0dd871b11f0beba43cb7aa3fbc01b01798dae607
nandinisharma225 <nandini1951.be20@chitkara.edu.in>
2022-08-16 09:59:24 +0530

    16Aug2022

---
+++ 16-Aug-2022/script.js
@@ -0,0 +1,12 @@
+// function check(myform) {
+//   if (myform.myname.value == "" || myform.myname.value == null) {
+//     alert("Name is mandatory");
+//     return false;
+//   }
+//   if (myform.myphone.value == "" || myform.myphone.value == null) {
+//     alert("Phone number is mandatory");
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
