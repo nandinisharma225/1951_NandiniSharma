4967f51b3534c318c7f3ecf1a2fc3572d6f77a35
nandinisharma225 <nandini1951.be20@chitkara.edu.in>
2022-08-23 13:11:51 +0530

    23Aug-hw

---
+++ 23Aug2022_hw/script.js
@@ -0,0 +1,31 @@
+var employees = [];
+
+function myfunc() {
+  let nameinp = document.getElementById("myinp").value;
+
+  employees.push({ name: "George", age: 32, retiredate: "March 12, 2014" });
+  employees.push({ name: "Edward", age: 17, retiredate: "June 2, 2023" });
+  employees.push({
+    name: "Christine",
+    age: 58,
+    retiredate: "December 20, 2036",
+  });
+  employees.push({ name: "Sarah", age: 62, retiredate: "April 30, 2020" });
+
+  employees.sort(function (a, b) {
+    return a.age - b.age;
+  });
+
+  // console.log(nameinp);
+  for (let i = 0; i < 4; i++) {
+    if (employees[i].age == nameinp) {
+      console.log(
+        employees[i].name +
+          " " +
+          employees[i].age +
+          " " +
+          employees[i].retiredate
+      );
+    }
+  }
+}
