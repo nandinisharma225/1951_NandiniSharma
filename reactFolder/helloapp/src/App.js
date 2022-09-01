113e8962deea11ec93f307b231bb75e14c1a2746
nandinisharma225 <nandini1951.be20@chitkara.edu.in>
2022-09-01 14:18:16 +0530

    reactFolder

---
+++ reactFolder/helloapp/src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <header className="container">
+      <Header />
+      <h1>Hello World</h1>
+    </header>
+  );
+}
+
+export default App;
